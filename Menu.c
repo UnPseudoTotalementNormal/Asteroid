a9f015142100c00a8961cf1ab18bd977ec8ca9cd
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2023-11-13 16:29:41 +0100

    main menu visuals done (not clickable)

--- Menu.c
+++ Menu.c
@@ -9,6 +9,36 @@
 #include "Menu.h"
 
 void draw_main_menu(sfRenderWindow* window, sfFont* font) {
-	
+    int WINDOW_X = sfVideoMode_getDesktopMode().width;
+    float ratio_x = WINDOW_X / 2560;
+
+    sfText* title_text = sfText_create();
+    sfText_setFont(title_text, font);
+    sfText_setString(title_text, "Shotgunnin' Space");
+    sfText_setCharacterSize(title_text, 120 * ratio_x);
+    sfText_setPosition(title_text, (sfVector2f) { -sfText_getLocalBounds(title_text).width / 2 + WINDOW_X / 2, 50 * ratio_x });
+
+    sfText* singleplayer_text = sfText_create();
+    sfText_setFont(singleplayer_text, font);
+    sfText_setString(singleplayer_text, "SinglePlayer");
+    sfText_setCharacterSize(singleplayer_text, 80 * ratio_x);
+    sfText_setPosition(singleplayer_text, (sfVector2f) { -sfText_getLocalBounds(singleplayer_text).width / 2 + WINDOW_X / 2, 700 * ratio_x });
+
+    sfText* multiplayer_text = sfText_create();
+    sfText_setFont(multiplayer_text, font);
+    sfText_setString(multiplayer_text, "MultiPlayer");
+    sfText_setCharacterSize(multiplayer_text, 80 * ratio_x);
+    sfText_setPosition(multiplayer_text, (sfVector2f) { -sfText_getLocalBounds(multiplayer_text).width / 2 + WINDOW_X / 2, 950 * ratio_x });
+
+    sfText* quit_text = sfText_create();
+    sfText_setFont(quit_text, font);
+    sfText_setString(quit_text, "Quit");
+    sfText_setCharacterSize(quit_text, 80 * ratio_x);
+    sfText_setPosition(quit_text, (sfVector2f) { -sfText_getLocalBounds(quit_text).width / 2 + WINDOW_X / 2, 1150 * ratio_x });
+
+    sfRenderWindow_drawText(window, singleplayer_text, NULL);
+    sfRenderWindow_drawText(window, multiplayer_text, NULL);
+    sfRenderWindow_drawText(window, quit_text, NULL);
+    sfRenderWindow_drawText(window, title_text, NULL);
 }
 
